e5e1d1d4e6081e2291006cdfdb53cc3d2a30635c
Reander Agulto <reander.agulto@oqulo.com>
2022-08-17 10:02:15 +0800

    Initial Commit

---
+++ aios-gutenberg.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Plugin Name:       AIOS Gutenberg
+ * Description:       Example static block scaffolded with Create Block tool.
+ * Requires at least: 5.9
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       aios-gutenberg
+ *
+ * @package           create-block
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_aios_gutenberg_block_init() {
+	register_block_type( __DIR__ . '/build/communities' );
+	register_block_type( 
+		__DIR__ . '/build/listings',
+		array(
+			'attributes'	=> array(
+				'selected_theme' 	=> array('type' => 'string',  'default' => 'default'),
+				'selected_view' 	=> array('type' => 'string',  'default' => 'grid'),
+				'posts_per_page'  	=> array('type' => 'number',  'default' => 4),
+				'featured_only'		=> array('type' => 'boolean', 'default' => false)
+			),
+			'render_callback' 	=> 'render_listing_block'
+		)
+	);
+}
+add_action( 'init', 'create_block_aios_gutenberg_block_init' );
+
+/**
+ * This function is called when the block is being rendered on the front end of the site
+ *
+ * @param array    $attributes     The array of attributes for this block.
+ * @param string   $content        Rendered block output. ie. <InnerBlocks.Content />.
+ * @param WP_Block $block_instance The instance of the WP_Block class that represents the block being rendered.
+ */
+function render_listing_block( $attributes, $content, $block_instance ){
+	ob_start();
+	/**
+	 * Keeping the markup to be returned in a separate file is sometimes better, especially if there is very complicated markup.
+	 * All of passed parameters are still accessible in the file.
+	 */
+	print_r($attributes)
+	return ob_get_clean();
+}
---
+++ src/listings/helper/edit.js
@@ -0,0 +1,130 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { 
+	useBlockProps,
+	InspectorControls
+} from '@wordpress/block-editor';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+
+import { 
+    Placeholder, 
+    PanelBody, 
+    RangeControl, 
+    SelectControl,
+    CheckboxControl
+} from '@wordpress/components';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import '../assets/editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit( props ) {
+	const { className, attributes, setAttributes } = props;
+    const blockProps = useBlockProps();
+
+    function updateSelectedTheme( val ) {
+        props.setAttributes( { selected_theme: val } );
+    }
+    function updateNoPost( val ) {
+        props.setAttributes( { posts_per_page: val } );
+    }
+    function updateFeaturedOnly( val ) {
+        props.setAttributes( { featured_only: val } );
+    }
+
+	return (
+		<div { ...blockProps }>
+            <InspectorControls>
+                <PanelBody
+                    title={ __('Listings Settings') }
+                    initialOpen={ true }
+                    className="aios-block-container"
+                >
+                    <fieldset className="aios-form-group">
+                        <div class="aios-block-col">
+                            <legend for="selectedTheme">
+                                { __( 'Select Theme:', 'aios-listings' ) }
+                            </legend>
+                        </div>
+                        <div class="aios-block-col">
+                            <SelectControl
+                                className="aios-form-control"
+                                name="selectedTheme"
+                                id="selectedTheme"
+                                value={ attributes.selected_theme }
+                                options={ [
+                                    { label: 'Classic', value: 'classic' },
+                                    { label: 'Default', value: 'default' },
+                                ] }
+                                onChange={ ( val ) => updateSelectedTheme(val) }
+                            />
+                        </div>
+                    </fieldset>
+                    <fieldset className="aios-form-group">
+                        <div class="aios-block-col">
+                            <legend for="numberOfPost">
+                                { __( 'Number of Post:', 'aios-listings' ) }
+                            </legend>
+                        </div>
+                        <div class="aios-block-col">
+                            <RangeControl
+                                value={ attributes.posts_per_page }
+                                className="aios-form-control"
+                                name="numberOfPost"
+                                id="numberOfPost"
+                                min={ 1 }
+                                max={ 10 }
+                                onChange={ ( val ) => updateNoPost(val) }
+                            />
+                        </div>
+                    </fieldset>
+                    <fieldset className="aios-form-group aios-checkbox">
+                        <div class="aios-block-col">
+                            <legend for="numberOfPost">
+                                { __( 'Featured Only?', 'aios-listings' ) }
+                            </legend>
+                        </div>
+                        <div class="aios-block-col">
+                            <CheckboxControl
+                                name="featuredOnly"
+                                id="featuredOnly"                               
+                                className="aios-form-control"
+                                checked={ attributes.featured_only }                        
+                                onChange={ ( val ) => updateFeaturedOnly(val) }
+                            />
+                        </div>
+                    </fieldset>
+                </PanelBody>
+            </InspectorControls>
+        </div>
+	);
+}
