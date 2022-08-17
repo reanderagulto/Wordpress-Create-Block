f3dbb33e208cbbb060f1ec4f12790a78fc7e6262
Reander Agulto <reander.agulto@oqulo.com>
2022-08-17 10:05:44 +0800

    Updated Version and remove build/ folder in ignore list

---
+++ build/communities/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e7b34f894de75a09ee40');
---
+++ build/listings/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3927ec1356c52c2415ca');
