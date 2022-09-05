73a04d6c59e8c3dcdd3bfe79d3f1e46d37af3760
Reander Agulto <reanderagulto29@gmail.com>
2022-09-06 07:48:32 +0800

    Convert Font Sizes from Pixel to Viewpoint Width

--- build/listings/index.asset.php
+++ build/listings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'b9373e26c32eeff01914');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'aca0523f957153480d8b');
